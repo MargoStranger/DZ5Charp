0554e43587eee6f7ef78d040f8f384006c8d442a
MargoStranger <margosebastian13@gmail.com>
2023-06-25 20:36:27 +0300

    сделано 2 дз

---
+++ dz5_2/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных ИНДЕКСАХ.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+int[] array = GetArray(size);
+Console.WriteLine($"Сумму элементов, стоящих на нечётных индексах в массиве [{String.Join(", ", array)}] = {GetSumm(array)}");
+
+
+//Метод поиска суммы элементов, стоящих на нечётных индексах
+int GetSumm(int[] array)
+{
+    int Summ = 0;
+    for(int i = 0; i<size; i++)
+    {
+        Summ += i%2!=0 ? array[i] : 0;
+    }
+    return Summ;
+}
+
+
+
+
+// Функция заполнения массива
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(-100, 101);
+    }
+    return result;
+}
